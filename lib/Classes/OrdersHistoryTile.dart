2e7551f3bf6f8e5ebb1cf75418317db1c14000af
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2021-05-17 00:27:33 +0430

    stateful widget added to Order History Tile

---
+++ lib/Classes/OrdersHistoryTile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class OrderHistoryTile extends StatefulWidget {
+  const OrderHistoryTile({Key key}) : super(key: key);
+
+  @override
+  _OrderHistoryTileState createState() => _OrderHistoryTileState();
+}
+
+class _OrderHistoryTileState extends State<OrderHistoryTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
