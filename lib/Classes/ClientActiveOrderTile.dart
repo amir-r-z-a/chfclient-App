58b071655420e7c1669af9c2c95b34ff885affe5
arman heidari <armanartgallery@gmail.com>
2021-05-19 03:11:23 +0430

    scaffold removed

--- lib/Classes/ClientActiveOrderTile.dart
+++ lib/Classes/ClientActiveOrderTile.dart
@@ -10,83 +10,79 @@ class ClientActiveOrderTile extends StatefulWidget {
 class _ClientActiveOrderTileState extends State<ClientActiveOrderTile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 220,
-          margin: EdgeInsets.all(12),
-          decoration: BoxDecoration(border: Border.all()),
-          child: Column(
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 220,
+      margin: EdgeInsets.all(12),
+      decoration: BoxDecoration(border: Border.all()),
+      child: Column(
+        children: [
+          Container(
+              child: Row(
             children: [
               Container(
-                  child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(12, 12, 0, 0),
-                    height: 90,
-                    width: 90,
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                margin: EdgeInsets.fromLTRB(12, 12, 0, 0),
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              "restauraante Chamooli",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.calendar_today),
-                              Text("15/05/2020"),
-                              Icon(Icons.watch_later_outlined),
-                              Text("12:15")
-                            ],
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on),
-                            Text("velenjak , meydan Daneshjoo ...")
-                          ],
+                        Text(
+                          "restauraante Chamooli",
+                          style: TextStyle(fontSize: 20),
                         ),
                       ],
                     ),
-                  )
-                ],
-              )),
-              Container(
-                margin: EdgeInsets.fromLTRB(12, 18, 12, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Sum of Prices"),
-                    Text("105 \$"),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.calendar_today),
+                          Text("15/05/2020"),
+                          Icon(Icons.watch_later_outlined),
+                          Text("12:15")
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on),
+                        Text("velenjak , meydan Daneshjoo ...")
+                      ],
+                    ),
                   ],
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(12, 18, 12, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    //TODO
-                  },
-                  child: Text("View invoice"),
-                ),
-              ),
+              )
             ],
+          )),
+          Container(
+            margin: EdgeInsets.fromLTRB(12, 18, 12, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Sum of Prices"),
+                Text("105 \$"),
+              ],
+            ),
           ),
-        ),
+          Container(
+            margin: EdgeInsets.fromLTRB(12, 18, 12, 0),
+            child: ElevatedButton(
+              onPressed: () {
+                //TODO
+              },
+              child: Text("View invoice"),
+            ),
+          ),
+        ],
       ),
     );
   }
