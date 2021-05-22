766e2871bb3a0fd4e8d9a82aa3723cc6f4b487ae
arman heidari <armanartgallery@gmail.com>
2021-05-22 17:12:48 +0430

    DetailsCartTile.dart was added

--- lib/Screens/DetailsCartTile.dart
+++ lib/Screens/DetailsCartTile.dart
@@ -1,6 +1,11 @@
+import 'package:chfclient/Classes/ClientAccounts.dart';
+import 'package:chfclient/Classes/FinishedClientFoodTile.dart';
+import 'package:chfclient/Classes/RestaurantAccounts.dart';
 import 'package:flutter/material.dart';
 
 class DetailsCartTile extends StatefulWidget {
+  static int j;
+
   @override
   _DetailsCartTileState createState() => _DetailsCartTileState();
 }
@@ -11,7 +16,44 @@ class _DetailsCartTileState extends State<DetailsCartTile> {
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
-        children: [],
+        children: [
+          Column(
+            children: List.generate(
+              ClientAccounts.accounts[ClientAccounts.currentAccount]
+                      .cartList[DetailsCartTile.j].cartName.length -
+                  1,
+              (index)
+              {
+                print(ClientAccounts.accounts[ClientAccounts.currentAccount]
+                    .cartList[DetailsCartTile.j].cartName);
+                print((ClientAccounts.accounts[ClientAccounts.currentAccount]
+                    .cartList[DetailsCartTile.j].cartSum[index] /
+                    ClientAccounts.accounts[ClientAccounts.currentAccount]
+                        .cartList[DetailsCartTile.j].cartNum[index])
+                    .toString());
+                return FinishedClientFoodTile(
+                  ClientAccounts.accounts[ClientAccounts.currentAccount]
+                      .cartList[DetailsCartTile.j].cartName[index],
+                  (ClientAccounts.accounts[ClientAccounts.currentAccount]
+                              .cartList[DetailsCartTile.j].cartSum[index] /
+                          ClientAccounts.accounts[ClientAccounts.currentAccount]
+                              .cartList[DetailsCartTile.j].cartNum[index])
+                      .toString(),
+                  RestaurantAccounts.restaurantList[0][DetailsCartTile.j]
+                      .getCategory(ClientAccounts
+                          .accounts[ClientAccounts.currentAccount]
+                          .cartList[DetailsCartTile.j]
+                          .cartName[index]),
+                  desc: RestaurantAccounts.restaurantList[0][DetailsCartTile.j]
+                      .getDesc(ClientAccounts
+                          .accounts[ClientAccounts.currentAccount]
+                          .cartList[DetailsCartTile.j]
+                          .cartName[index]),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
